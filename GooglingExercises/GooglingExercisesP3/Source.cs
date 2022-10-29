5c3a580867db966c31c366423b8dee0ce77e1b23
SH-NicholasPage <npage@southhills.edu>
2022-10-29 01:28:19 -0400

    Added third project

---
+++ GooglingExercises/GooglingExercisesP3/Source.cs
@@ -0,0 +1,46 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+* Link(s) Used: [INSERT LINK(S) HERE]
+*/
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace GooglingExercisesP3
+{
+    public class Source
+    {
+        public static Type GetCalculatorType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Calculator CreateCalculatorUsingReflection()
+        {
+            Calculator calc = (Source)Activator.CreateInstance(typeof(Source))!;
+            return calc;
+        }
+
+        //Get the number of properties in the Calculator class.
+        //Do not return a hard-coded value.
+        public static int GetNumberOfPropertiesInCalculator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (double number, double pi, double tau, double e) GetCalculatorPropertyValues(Object obj)
+        {
+            Calculator calc = (Calculator)obj;
+
+            return (default(double), default(double), default(double), default(double));
+        }
+
+        //Return the line number of the return statement.
+        public static int GetLineNumber()
+        {
+            return default(int);//Return the line number
+        }
+    }
+}
--- GooglingExercises/GooglingExercisesPt2/Source.cs
+++ GooglingExercises/GooglingExercisesPt2/Source.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+* Link(s) Used: [INSERT LINK(S) HERE]
+*/
+
+using System;
 using System.Collections.Generic;
 
 namespace GooglingExercisesPt2
