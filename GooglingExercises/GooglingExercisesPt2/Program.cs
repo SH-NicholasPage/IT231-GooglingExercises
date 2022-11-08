bdb4e54808f765fb4270a6c80b87cbf6cea6922d
wheat32 <npage@southhills.edu>
2022-11-08 12:14:08 -0500

    Fixed bug in pt2 and typo in the name of pt3

--- GooglingExercises/GooglingExercisesPt2/Program.cs
+++ GooglingExercises/GooglingExercisesPt2/Program.cs
@@ -8,13 +8,14 @@ namespace GooglingExercisesPt2
     {
         public static void Main()
         {
-            List<Int32> list = new List<Int32>(Enumerable.Range(0, 10).Select(x => x = Random.Shared.Next(1, 10)));
+            List<Int32> list = new List<Int32>(Enumerable.Range(0, 15).Select(x => x = Random.Shared.Next(1, 10)));
             int numToRemove = Random.Shared.Next(1, 10);
+            list.Insert(Random.Shared.Next(0, list.Count), numToRemove);
             Source.RemoveNumbers(list, numToRemove);
 
             bool numExists = list.Where(x => x == numToRemove).Count() > 1;
 
-            Console.WriteLine(numExists ? "Correct!" : "Incorrect - " + numToRemove + " still exists in the list.");
+            Console.WriteLine(numExists == false ? "Correct!" : "Incorrect - " + numToRemove + " still exists in the list.");
         }
     }
 }
